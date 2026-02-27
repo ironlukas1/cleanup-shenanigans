386f95174ec39f037221b14934a90426d7909d5c
lunix <ironlukas1@gmail.com>
2026-02-28 00:50:38 +0100

    added simple movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 inputDirection; 
     void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        //GetComponent<Rigidbody>().rotation = Quaternion.Euler(0, 45, 0); // x y z
     }
 
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        inputDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        //rb.MoveRotation(Quaternion.LookRotation(inputDirection));
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(inputDirection * moveSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        //rigidbody.MoveRotation(Quaternion.LookRotation(rigidbody.velocity));
     }
 }
